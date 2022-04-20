5da3e714232f9db97a33e6b9c89a261a78780125
tanujjoshiji <104056354+tanujjoshiji@users.noreply.github.com>
2022-04-20 13:01:53 +0530

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,24 @@
+function btn() {
+ window.location.href="https://mail.google.com/mail/u/0/#inbox/FMfcgzGllCcMpZLkpdfDwJmXzBjSFVCR";
+ window.location.replace="file:///D:/web%20devlopment/PROJECT2022/bio.html";
+} 
+function navbarbtn() {
+ var x=document.querySelector(".list");
+  if(x.style.display=="block"){
+      x.style.display="none";
+  }
+  else{
+    x.style.display="block";
+
+  }
+   } 
+   function btnA() {
+    window.location.href="https://www.youtube.com/"
+   }
+   function btnB() {
+    window.location.href="https://fontawesome.com/"
+   }
+   function btnC() {
+    window.location.href="https://www.w3schools.com/"
+   }
+    
